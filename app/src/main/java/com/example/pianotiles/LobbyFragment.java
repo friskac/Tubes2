f53e25da09be53f035a9d7972cb2ec2ca99fc780
denisestevani <56673734+denisestevani@users.noreply.github.com>
2020-11-20 23:38:41 +0700

    [Add]
    button dan textView buat lobby

--- app/src/main/java/com/example/pianotiles/LobbyFragment.java
+++ app/src/main/java/com/example/pianotiles/LobbyFragment.java
@@ -1,9 +1,18 @@
 package com.example.pianotiles;
 
 import androidx.fragment.app.Fragment;
+import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class LobbyFragment extends Fragment {
+    protected Button btnPlay;
+    protected Button btnSetting;
+    protected Button btnHS;
+    protected Button btnExit;
+
     public LobbyFragment(){
+
 
     }
 }
