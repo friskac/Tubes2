3c6332fcd58346dcae391b56c5f3e67b3a4ac6c0
denisestevani <56673734+denisestevani@users.noreply.github.com>
2020-11-20 23:47:01 +0700

    [EDIT]
    edit lobby fragment

--- app/src/main/java/com/example/pianotiles/LobbyFragment.java
+++ app/src/main/java/com/example/pianotiles/LobbyFragment.java
@@ -1,6 +1,9 @@
 package com.example.pianotiles;
 
 import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.widget.Button;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,42 @@ public class LobbyFragment extends Fragment {
 
     public LobbyFragment(){
 
-
     }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lobby,container,false);
+        this.btnPlay = view.findViewById(R.id.btn_play);
+        this.btnHS = view.findViewById(R.id.btn_score);
+        this.btnSetting = view.findViewById(R.id.btn_setting);
+        this.btnExit = view.findViewById(R.id.btn_exit);
+
+        this.btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        this.btnHS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        this.btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        this.btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return view;
+    }
+
 }
