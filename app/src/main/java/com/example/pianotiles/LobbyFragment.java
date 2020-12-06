fe7c52caa2774869109054c37736b5dca8a5e1ab
friskac <christianafriska@gmail.com>
2020-12-06 16:49:44 +0700

    [DELETE] semua ttg setting

--- app/src/main/java/com/example/pianotiles/LobbyFragment.java
+++ app/src/main/java/com/example/pianotiles/LobbyFragment.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 
 public class LobbyFragment extends Fragment {
     protected Button btnPlay;
-    protected Button btnSetting;
     protected Button btnHS;
     protected Button btnExit;
     protected FragmentType fragmentType;
